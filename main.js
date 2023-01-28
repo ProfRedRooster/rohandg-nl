b927db48283a01e3a89b8b9868aaef40c74713a7
Rohan De graaf <52485505+ProfRedRooster@users.noreply.github.com>
2023-01-28 18:27:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,110 +1,120 @@
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 33,
-      "density": {
-        "enable": true,
-        "value_area": 1420.4657549380909
+// let it rain
+(function(){
+  var counter = document.querySelector('.counter');
+  var diner = document.querySelector('h1');
+  var menu = ['🍔', '🌭', '🍕', '🍗', '🌮', '🍔'];
+  var noms = ['nom', 'nom!', 'nom!!', 'nommm', 'nommy'];
+  var finished = 0;
+
+  // animation to play when finished dining
+  function jelly(e) {
+    diner.removeEventListener('fit', jelly);
+    diner.animate(
+      [
+        { transform: 'scale(' + 1 / e.detail.scaleFactor + ')' },
+        { transform: 'scale(1.1)' },
+        { transform: 'scale(.9)' },
+        { transform: 'scale(1.05)' },
+        { transform: 'scale(.98)' },
+        { transform: 'scale(1)' }
+      ],
+      {
+        duration:500
       }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "triangle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+    );
+  }
+
+  // eat animation
+  function nom(index) {
+
+    var nommer = document.getElementById('nommer-' + index);
+    var rotation = -20 + (Math.random() * 40);
+    var scale = .75 + (Math.random() * .5);
+
+    nommer.animate(
+      [
+        { opacity:0, transform: 'scale(' + .25 * scale + ') rotateZ(' + rotation + 'deg) translateY(0) ' },
+        { opacity:1, transform: 'scale(' + scale + ') rotateZ(' + rotation + 'deg) translateY(-.5em) ' }
+      ],
+      {
+        duration:250
       }
-    },
-    "opacity": {
-      "value": 0.06313181133058181,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
+    );
+
+    diner.animate(
+      [
+        { transform: 'scaleY(1)' },
+        { transform: 'scaleY(' + (.7 + (Math.random() * .2)) + ')' },
+        { transform: 'scaleY(1)' }
+      ],
+      {
+        duration:100
       }
-    },
-    "size": {
-      "value": 11.83721462448409,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+    );
+  }
+
+  // only when animation api is supported
+  if ('animate' in diner) {
+    diner.addEventListener('fit', jelly);
+  }
+
+  // create food items
+  var dishes = menu
+  .concat(menu) // duplicate icons
+  .sort(function() { return .5 - Math.random(); })
+  .concat(['🐝']) // unfortunately there's no fly emoji
+  .map(function(menuItem, i) {
+
+    var offset = 33.33 + (Math.random() * 33.33);
+
+    var dish = document.createElement('div');
+    dish.textContent = menuItem;
+    dish.setAttribute('aria-hidden', 'true');
+    dish.setAttribute('data-nommer', i);
+    dish.className = 'dish';
+    dish.style.cssText = 'left:' + offset + '%; transition-delay: ' + ((i * 200) + Math.random() * 100) + 'ms';
+
+    var nommer = document.createElement('div');
+    nommer.id = 'nommer-' + i;
+    nommer.textContent = noms[Math.floor(Math.random() * noms.length)];
+    nommer.setAttribute('aria-hidden', 'true');
+    nommer.className = 'nommer';
+    nommer.style.cssText = 'left:' + offset + '%; opacity:0;';
+
+    counter.appendChild(dish);
+    counter.appendChild(nommer);
+
+    return dish;
+  });
+
+  // animate into view
+  setTimeout(function() {
+    requestAnimationFrame(function() {
+      dishes.forEach(function (dish) {
+        dish.classList.add('drop');
+        dish.addEventListener('transitionend', burb);
+      })
+    });
+  }, 500);
+
+  // burb after eating dish
+  function burb(e) {
+    e.target.removeEventListener('transitionend', burb);
+    e.target.style.opacity = 0;
+
+    finished++;
+    if (finished === dishes.length) {
+      full();
     }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
+    else if ('animate' in diner) {
+      nom(e.target.getAttribute('data-nommer'));
     }
-  },
-  "retina_detect": true
-});
+  }
+
+  // so full!
+  function full() {
+    fitty(diner, {
+      minSize:64
+    });
+  }
+}());
